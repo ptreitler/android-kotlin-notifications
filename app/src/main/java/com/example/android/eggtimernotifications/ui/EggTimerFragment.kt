ed4b76383b491866d155cd3ab5e880050f6fe762
Peter Treitler <peter.treitler@axtesys.at>
2019-11-12 15:31:32 +0100

    Disable notification badges

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -71,6 +71,7 @@ class EggTimerFragment : Fragment() {
             lightColor = Color.RED
             enableVibration(true)
             description = "Time for breakfast"
+            setShowBadge(false)
         }
 
         getNotificationManager(requireContext()).createNotificationChannel(notificationChannel)
