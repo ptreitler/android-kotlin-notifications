65c31b8f24caa1c1c9d5556c58958f03afc4d49a
Peter Treitler <peter.treitler@axtesys.at>
2019-11-18 16:18:05 +0100

    Display notification from onMessageReceived

--- app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -1,6 +1,8 @@
 package com.example.android.eggtimernotifications
 
 import android.util.Log
+import com.example.android.eggtimernotifications.util.getNotificationManager
+import com.example.android.eggtimernotifications.util.sendNotification
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -16,6 +18,15 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         Log.d(TAG, "From: ${remoteMessage.from}")
         Log.d(TAG, "Message data payload: " + remoteMessage.data)
+
+        remoteMessage.notification?.body?.let {
+            Log.d(TAG, "Message notification body: $it")
+            sendNotification(it)
+        }
+    }
+
+    private fun sendNotification(messageBody: String) {
+        getNotificationManager(applicationContext).sendNotification(messageBody, applicationContext)
     }
 
     private fun sendRegistrationToServer(@Suppress("UNUSED_PARAMETER") token: String) {
