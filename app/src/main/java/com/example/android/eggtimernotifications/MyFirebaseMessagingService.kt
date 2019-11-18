5bc34edf934dc7c4846a4ff0882b2615b73c050c
Peter Treitler <peter.treitler@axtesys.at>
2019-11-18 16:12:51 +0100

    Log message payload in MyFirebaseMessagingService

--- app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -2,6 +2,7 @@ package com.example.android.eggtimernotifications
 
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 private val TAG = MyFirebaseMessagingService::class.java.simpleName
 
@@ -10,6 +11,11 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         Log.d(TAG, "Refreshed token: $token")
         sendRegistrationToServer(token)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "From: ${remoteMessage.from}")
+        Log.d(TAG, "Message data payload: " + remoteMessage.data)
     }
 
     private fun sendRegistrationToServer(@Suppress("UNUSED_PARAMETER") token: String) {
